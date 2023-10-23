d51ae24360096512a7612ccd31411287920e6beb
FonDitbul <niki7084@naver.com>
2023-10-23 15:35:20 +0900

    [Refactoring-chapter_01] parameter 변경하기

--- book/리팩터링_2판/chapter_01/statement_my.js
+++ book/리팩터링_2판/chapter_01/statement_my.js
@@ -7,9 +7,9 @@ export function statement_my(invoice, plays) {
         const play = plays[perf.playID];
         let thisAmount = 0;
 
-        thisAmount = calculateAmount(play, thisAmount, perf);
+        thisAmount = calculateAmount(play.type, thisAmount, perf.audience);
 
-        volumeCredits += stackVolumeCredits(perf, play)
+        volumeCredits += stackVolumeCredits(perf.audience, play.type)
 
         // 청구 내역을 출력한다.
         result += billingDetail(play.name, thisAmount, perf.audience)
@@ -25,38 +25,38 @@ function billingDetail(playName, amount, audience) {
     return `${playName}: ${amount / 100} ${audience}석\n`
 }
 
-function stackVolumeCredits (perf, play) {
+function stackVolumeCredits (perfAudience, playType) {
     // 포인트를 적립한다.
     let volumeCredit = 0
-    volumeCredit += Math.max(perf.audience - 30, 0);
+    volumeCredit += Math.max(perfAudience - 30, 0);
 
     // 희극 관객 5명마다 추가 포인트를 제공한다.
-    if ('comedy' === play.type) {
-        volumeCredit += Math.floor(perf.audience / 5);
+    if ('comedy' === playType) {
+        volumeCredit += Math.floor(perfAudience / 5);
     }
     return volumeCredit
 }
 
-function calculateAmount(play, thisAmount, perf) {
-    switch (play.type) {
+function calculateAmount(playType, thisAmount, perfAudience) {
+    switch (playType) {
         case 'tragedy':
             thisAmount = 40_000;
 
-            if (perf.audience > 30) {
-                thisAmount += 1_000 * (perf.audience - 30);
+            if (perfAudience > 30) {
+                thisAmount += 1_000 * (perfAudience - 30);
             }
             break;
         case 'comedy':
             thisAmount = 30_000;
 
-            if (perf.audience > 20) {
-                thisAmount += 10_000 + 500 * (perf.audience - 20);
+            if (perfAudience > 20) {
+                thisAmount += 10_000 + 500 * (perfAudience - 20);
             }
-            thisAmount += 300 * perf.audience;
+            thisAmount += 300 * perfAudience;
             break;
 
         default:
-            throw new Error(`알 수 없는 장르: ${play.type}`);
+            throw new Error(`알 수 없는 장르: ${playType}`);
     }
     return thisAmount;
 }
