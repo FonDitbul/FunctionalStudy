f05b2cb872b1f24a30f40b42732853a9d4d896c9
FonDitbul <niki7084@naver.com>
2023-10-23 15:49:01 +0900

    [Refactoring-chapter_01] play 함수 변경

--- book/리팩터링_2판/chapter_01/statement_my.js
+++ book/리팩터링_2판/chapter_01/statement_my.js
@@ -1,69 +1,73 @@
 export function statement_my(invoice, plays) {
-    let result = `청구내역 (고객명: ${invoice.customer})\n`;
+  let result = `청구내역 (고객명: ${invoice.customer})\n`;
 
-    // 가격을 계산한다.
-    let totalAmount = 0;
-    for (let perf of invoice.performances) {
-        const play = plays[perf.playID];
-        totalAmount += calculateAmount(play.type, perf.audience);
-    }
+  // 가격을 계산한다.
+  let totalAmount = 0;
+  for (let perf of invoice.performances) {
+    const play = getPlay(plays, perf.playID);
+    totalAmount += calculateAmount(play.type, perf.audience);
+  }
 
-    // 포인트를 적립한다.
-    let volumeCredits = 0;
-    for (let perf of invoice.performances) {
-        const play = plays[perf.playID];
-        volumeCredits += stackVolumeCredits(perf.audience, play.type)
-    }
+  // 포인트를 적립한다.
+  let volumeCredits = 0;
+  for (let perf of invoice.performances) {
+    const play = getPlay(plays, perf.playID);
+    volumeCredits += stackVolumeCredits(perf.audience, play.type);
+  }
 
-    // 청구 내역을 출력한다.
-    for (let perf of invoice.performances) {
-        const play = plays[perf.playID];
-        const thisAmount = calculateAmount(play.type, perf.audience);
-        result += billingDetail(play.name, thisAmount, perf.audience)
-    }
+  // 청구 내역을 출력한다.
+  for (let perf of invoice.performances) {
+    const play = getPlay(plays, perf.playID);
+    const thisAmount = calculateAmount(play.type, perf.audience);
+    result += billingDetail(play.name, thisAmount, perf.audience);
+  }
 
-    result += `총액 ${totalAmount / 100}\n`;
-    result += `적립 포인트 ${volumeCredits}점\n`;
-    return result;
+  result += `총액 ${totalAmount / 100}\n`;
+  result += `적립 포인트 ${volumeCredits}점\n`;
+  return result;
+}
+
+function getPlay(plays, perfPlayID) {
+  return plays[perfPlayID];
 }
 
 function billingDetail(playName, amount, audience) {
-    return `${playName}: ${amount / 100} ${audience}석\n`
+  return `${playName}: ${amount / 100} ${audience}석\n`;
 }
 
-function stackVolumeCredits (perfAudience, playType) {
-    // 포인트를 적립한다.
-    let volumeCredit = 0
-    volumeCredit += Math.max(perfAudience - 30, 0);
+function stackVolumeCredits(perfAudience, playType) {
+  // 포인트를 적립한다.
+  let volumeCredit = 0;
+  volumeCredit += Math.max(perfAudience - 30, 0);
 
-    // 희극 관객 5명마다 추가 포인트를 제공한다.
-    if ('comedy' === playType) {
-        volumeCredit += Math.floor(perfAudience / 5);
-    }
-    return volumeCredit
+  // 희극 관객 5명마다 추가 포인트를 제공한다.
+  if ('comedy' === playType) {
+    volumeCredit += Math.floor(perfAudience / 5);
+  }
+  return volumeCredit;
 }
 
-function calculateAmount(playType,  perfAudience) {
-    let thisAmount = 0;
-    switch (playType) {
-        case 'tragedy':
-            thisAmount = 40_000;
+function calculateAmount(playType, perfAudience) {
+  let thisAmount = 0;
+  switch (playType) {
+    case 'tragedy':
+      thisAmount = 40_000;
 
-            if (perfAudience > 30) {
-                thisAmount += 1_000 * (perfAudience - 30);
-            }
-            break;
-        case 'comedy':
-            thisAmount = 30_000;
+      if (perfAudience > 30) {
+        thisAmount += 1_000 * (perfAudience - 30);
+      }
+      break;
+    case 'comedy':
+      thisAmount = 30_000;
 
-            if (perfAudience > 20) {
-                thisAmount += 10_000 + 500 * (perfAudience - 20);
-            }
-            thisAmount += 300 * perfAudience;
-            break;
+      if (perfAudience > 20) {
+        thisAmount += 10_000 + 500 * (perfAudience - 20);
+      }
+      thisAmount += 300 * perfAudience;
+      break;
 
-        default:
-            throw new Error(`알 수 없는 장르: ${playType}`);
-    }
-    return thisAmount;
+    default:
+      throw new Error(`알 수 없는 장르: ${playType}`);
+  }
+  return thisAmount;
 }
