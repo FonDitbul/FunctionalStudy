e1f2fe7e1592a7c4e5091b541bb084e8fcf42cfb
FonDitbul <niki7084@naver.com>
2023-10-27 02:15:15 +0900

    [Refactoring-chapter_01] 조건부 로직을 다형성으로 바꾸기

--- book/리팩터링_2판/chapter_01/Calculator.js
+++ book/리팩터링_2판/chapter_01/Calculator.js
@@ -1,43 +1,52 @@
+export function createCalculator(play, performance) {
+  switch (play.type) {
+    case 'tragedy':
+      return new TragedyCalculator(performance);
+    case 'comedy':
+      return new ComedyCalculator(performance);
+    default:
+      throw new Error(`알 수 없는 장르: ${play.type}`);
+  }
+}
+
 export class Calculator {
-  constructor(performance, play) {
+  constructor(performance) {
     this.performance = performance;
-    this.play = play;
   }
 
+  get amount() {}
+
+  get volumeCredits() {}
+}
+
+export class TragedyCalculator extends Calculator {
   get amount() {
-    let thisAmount = 0;
-    switch (this.play.type) {
-      case 'tragedy':
-        thisAmount = 40_000;
+    let thisAmount = 40_000;
 
-        if (this.performance.audience > 30) {
-          thisAmount += 1_000 * (this.performance.audience - 30);
-        }
-        break;
-      case 'comedy':
-        thisAmount = 30_000;
-
-        if (this.performance.audience > 20) {
-          thisAmount += 10_000 + 500 * (this.performance.audience - 20);
-        }
-        thisAmount += 300 * this.performance.audience;
-        break;
-
-      default:
-        throw new Error(`알 수 없는 장르: ${this.play.type}`);
+    if (this.performance.audience > 30) {
+      thisAmount += 1_000 * (this.performance.audience - 30);
     }
+
+    return thisAmount;
+  }
+  get volumeCredits() {
+    return Math.max(this.performance.audience - 30, 0);
+  }
+}
+
+export class ComedyCalculator extends Calculator {
+  get amount() {
+    let thisAmount = 30_000;
+
+    if (this.performance.audience > 20) {
+      thisAmount += 10_000 + 500 * (this.performance.audience - 20);
+    }
+    thisAmount += 300 * this.performance.audience;
+
     return thisAmount;
   }
 
   get volumeCredits() {
-      // 포인트를 적립한다.
-      let volumeCredit = 0;
-      volumeCredit += Math.max(this.performance.audience - 30, 0);
-
-      // 희극 관객 5명마다 추가 포인트를 제공한다.
-      if ('comedy' === this.performance.type) {
-          volumeCredit += Math.floor(this.performance.audience / 5);
-      }
-      return volumeCredit;
+    return Math.max(this.performance.audience - 30, 0) + Math.floor(this.performance.audience / 5);
   }
 }
--- book/리팩터링_2판/chapter_01/statement_my.js
+++ book/리팩터링_2판/chapter_01/statement_my.js
@@ -1,22 +1,16 @@
-import { Calculator } from './Calculator';
+import { createCalculator } from './Calculator';
 
 export function statement_my(invoices, plays) {
   let result = `청구내역 (고객명: ${invoices.customer})\n`;
 
-  // amount 를 포함한 객체 생성
   const resultData = Object.assign({}, invoices);
-  resultData.performances = invoices.performances.map((perf) => ({
-    ...perf,
-    amount: calculateAmount(plays, perf),
-    type: getPlayType(plays, perf),
-    name: getPlayName(plays, perf),
-  }));
+  resultData.performances = invoices.performances.map((perf) => enrichStatementData(plays, perf));
 
   // 가격을 계산한다.
   const totalAmount = sumTotalAmount(resultData.performances);
 
   // 포인트를 적립한다.
-  const volumeCredits = sumVolumeCredits(resultData.performances);
+  const volumeCredits = resultData.performances.reduce((prev, cur) => prev + cur.credits, 0);
 
   // 청구 내역을 출력한다.
   for (let perf of resultData.performances) {
@@ -32,11 +26,6 @@ function getPlay(plays, perf) {
   return plays[perf.playID];
 }
 
-function getPlayType(plays, perf) {
-  const play = getPlay(plays, perf);
-  return play.type;
-}
-
 function getPlayName(plays, perf) {
   const play = getPlay(plays, perf);
   return play.name;
@@ -46,30 +35,19 @@ function billingDetail(perf) {
   return `${perf.name}: ${perf.amount / 100} ${perf.audience}석\n`;
 }
 
-function stackVolumeCredits(perf) {
-  // 포인트를 적립한다.
-  let volumeCredit = 0;
-  volumeCredit += Math.max(perf.audience - 30, 0);
-
-  // 희극 관객 5명마다 추가 포인트를 제공한다.
-  // TODo 변경
-  if ('comedy' === perf.type) {
-    volumeCredit += Math.floor(perf.audience / 5);
-  }
-  return volumeCredit;
-}
-
-function calculateAmount(plays, perf) {
-  const play = getPlay(plays, perf);
-  const calculator = new Calculator(perf, play);
-
-  return calculator.amount;
-}
-
 function sumTotalAmount(performances) {
   return performances.reduce((prev, cur) => prev + cur.amount, 0);
 }
 
-function sumVolumeCredits(performances) {
-  return performances.reduce((prev, cur) => prev + new Calculator(cur).volumeCredits, 0);
+function enrichStatementData(plays, performance) {
+  const play = getPlay(plays, performance);
+
+  const calculator = createCalculator(play, performance);
+
+  const result = Object.assign({}, performance);
+  result.name = getPlayName(plays, performance);
+  result.amount = calculator.amount;
+  result.credits = calculator.volumeCredits;
+
+  return result;
 }
