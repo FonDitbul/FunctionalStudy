af4eaaca93f70efebe442c74a2931aac1e3611d1
FonDitbul <niki7084@naver.com>
2023-10-29 16:32:01 +0900

    [Refactoring-chapter_06] 함수 추출하기

---
+++ book/리팩터링_2판/chapter_06/extract_function.js
@@ -0,0 +1,22 @@
+// before
+function printOwing(invoice) {
+    printBanner();
+    let outstanding = calculateOutstanding();
+    
+    // 세부 사항 출력
+    console.log(`고객명: ${invoice.customer}`);
+    console.log(`채무액: ${outstanding}`)
+}
+
+// after
+function printOwing(invoice) {
+    printBanner();
+    let outstanding = calculateOutstanding();
+    printDetails(outstanding)
+
+    // 세부 사항 출력
+    function printDetails(outstanding) {
+        console.log(`고객명: ${invoice.customer}`);
+        console.log(`채무액: ${outstanding}`)    
+    }
+}
